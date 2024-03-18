8aaca55b451ca7a3abb248af4022d4610233d5df
Daniel Yaroshovets <dan.yaroshovets@gmail.com>
2024-03-18 20:32:17 +0100

    created task 8

---
+++ square_package/cubic_generator.py
@@ -0,0 +1,6 @@
+from square_package.square_generator import SquareGenerator
+
+class CubicGenerator(SquareGenerator):
+    def generate_cubes(self, start, end):
+        cubes = [x**3 for x in range(start, end + 1)]
+        return cubes
